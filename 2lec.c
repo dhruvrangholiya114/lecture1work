eeb98b341c281cdb21320004363986200a609dc5
dhruvrangholiya114 <130462930+dhruvrangholiya114@users.noreply.github.com>
2023-04-11 17:12:55 +0530

    Add files via upload

---
+++ 2lec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	printf(" -------\n");
+	printf("|\t|\n");
+	printf("D\t|\n");
+	printf("H\t|\n");
+	printf("R\t|\n");
+	printf("U\t|\n");
+	printf("V\t|\n");
+	printf("|\t|\n");
+	printf(" -------\n");
+	
+}
